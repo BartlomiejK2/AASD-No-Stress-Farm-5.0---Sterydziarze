21bb506850dd80b5ca26fb262e47f59449458579
Mateusz Damian Chmurzyski <m.chmurzynski@stud.elka.pw.edu.pl>
2025-12-30 13:46:29 +0100

    rem

--- src/cow_analysis/main.py
+++ src/cow_analysis/main.py
@@ -1,21 +1,12 @@
 import spade
 
 from agents.cows_analizer import CowsAnalyzer
-<<<<<<< HEAD:src/analysis/main.py
-from agents.spacial_analizer import SpacialAnalyzer
 
-=======
-from spatial_analysis.spatial_analizer import SpatialAnalyzer
->>>>>>> b90fe2c (analizator krow + przestrzenny):src/cow_analysis/main.py
 
 async def main():
     cows_analyser = CowsAnalyzer()
     await cows_analyser.start(auto_register=True)
     print("cows_analyser started")
-
-    spacial_analyser = SpacialAnalyzer()
-    await spacial_analyser.start(auto_register=True)
-    print("spacial_analyser started")
 
     with open("/tmp/agent_ready", "w") as f:
         f.write("ready")
